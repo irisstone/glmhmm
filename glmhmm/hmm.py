91762c08cb405a6d85c1c55dae83860385fe5ef9
irisstone <istone@princeton.edu>
2020-09-10 16:44:32 -0400

    added initialize_states and initialize_emissions functions

--- glmhmm/hmm.py
+++ glmhmm/hmm.py
@@ -11,6 +11,8 @@ Updated: Sep 1, 2020
 
 """
 
+import numpy as np
+
 class HMM(object):
 
     """
@@ -26,62 +28,109 @@ class HMM(object):
 
     """
     
-        def __init__(self,n,m,c):
+    def __init__(self,n,m,c,k):
             self.n, self.m, self.c, self.k  = n, m, c, k
             
-        def initialize_transitions(self,distribution='dirichlet',alpha_diag=5,alpha_full=1):
-            
-            """
-            Initializes values for the transition probabilities.
-    
-            Parameters
-            ----------
-            distribution : string, optional
-                Sets the distribution to use when initializing the transition probabilities. The default is dirichlet.
-            alpha_diag : int, optional
-                Sets the concentration parameter for the diagonal values when using a Dirichlet distribution. Default is 5. 
-            alpha_full : int, optional
-                Sets the concentration parameter for the off-diagonal values when using a Dirichlet distribution. Default is 1.
-    
-            Returns
-            -------
-            A : kxk matrix of the transition probabilities
-    
-            """
-            
-            if distribution == 'dirichlet':
-            
-                # Make transition matrix by sampling each row from Dirichlet distribution (achieved by normalizing gamma random variables)
-                A = np.random.gamma(alpha_full*np.ones((self.k,self.k)) + alpha_diag*np.identity(self.k),1)
-                A = A/np.repeat(np.reshape(np.sum(A,axis=1),(1,self.k)),self.k,0).T
-                
-            elif distribution == 'uniform':
-                
-                # Make transition matrix probabilities uniform 
-                A = (1/self.k) * np.ones((self.k,self.k))
-            
-            return A
-            
-        def initialize_params(self,alpha_diag,alpha_full,alpha_emissions,nStates,nObs,nT,method):
-            
-            return
-            
-        def generate_data(self,self.n,A,M):
+    def initialize_transitions(self,distribution='dirichlet',alpha_diag=5,alpha_full=1):
+        
+        """
+        Initializes values for the transition probabilities.
 
-            return
+        Parameters
+        ----------
+        distribution : string, optional
+            Sets the distribution to use when initializing the transition probabilities. The default is dirichlet.
+        alpha_diag : int, optional
+            Sets the concentration parameter for the diagonal values when using a Dirichlet distribution. Default is 5. 
+        alpha_full : int, optional
+            Sets the concentration parameter for the off-diagonal values when using a Dirichlet distribution. Default is 1.
+
+        Returns
+        -------
+        A : kxk matrix of the transition probabilities
+
+        """
         
-        def neglogli(self):
-            
-            return
-            
-        def forwardPass(self):
-            
-            return
+        if distribution == 'dirichlet':
         
-        def backwardPass(self):
+            # Make transition matrix by sampling each row from Dirichlet distribution (achieved by normalizing gamma random variables)
+            A = np.random.gamma(alpha_full*np.ones((self.k,self.k)) + alpha_diag*np.identity(self.k),1)
+            A = A/np.repeat(np.reshape(np.sum(A,axis=1),(1,self.k)),self.k,0).T
             
-            return
+        elif distribution == 'uniform':
+            
+            # Make transition matrix probabilities uniform 
+            A = (1/self.k) * np.ones((self.k,self.k))
         
-        def EM(self):
-            
-            return
+        return A
+    
+    def initialize_states(self,distribution='uniform'):
+        
+        """
+        Initializes values for the state probabilities at t=1.
+
+        Parameters
+        ----------
+        distribution : string, optional
+            Sets the distribution to use when initializing the state probabilities for t=1. The default is uniform (same probability for each state).
+
+        Returns
+        -------
+        A : kx1 vector of the state probabilities for t=1
+
+        """
+        
+        if distribution == 'uniform':
+            pi = (1/self.k) * np.ones((self.k,1))
+        
+        return pi
+    
+    def initialize_emissions(self,distribution='dirichlet',alpha_diag=5,alpha_full=1):
+        
+        """
+        Initializes values for the emission (observation) probabilities.
+
+        Parameters
+        ----------
+        distribution : string, optional
+            Sets the distribution to use when initializing the emission probabilities. The default is dirichlet.
+        alpha_diag : int, optional
+            Sets the concentration parameter for the diagonal values when using a Dirichlet distribution. Default is 5. 
+        alpha_full : int, optional
+            Sets the concentration parameter for the off-diagonal values when using a Dirichlet distribution. Default is 1.
+
+        Returns
+        -------
+        phi : nxc matrix of the emission probabilities
+
+        """
+        
+        if distribution == 'dirichlet':
+            # Make emissions matrix by sampling each row from Dirichlet distribution (achieved by normalizing gamma random variables)
+            phi = np.random.gamma(alpha_full,1,(self.c,self.k))
+            phi = phi + np.append(alpha_diag*np.identity(self.k), np.zeros((self.c-self.k,self.k)),axis=0) # add to diagonal
+            phi = phi/np.repeat(np.reshape(np.sum(phi,axis=0),(1,self.k)),self.c,0) # normalize so columns sum to 1
+        
+        return phi
+    
+
+        
+    def generate_data(self,A,M):
+
+        return
+    
+    def neglogli(self):
+        
+        return
+        
+    def forwardPass(self):
+        
+        return
+    
+    def backwardPass(self):
+        
+        return
+    
+    def EM(self):
+        
+        return

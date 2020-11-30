524387b426ab11012d0fa3fe67c4ede80f5c0f4f
irisstone <istone@princeton.edu>
2020-11-30 18:01:03 -0500

    M-step functions in progress

--- glmhmm/hmm.py
+++ glmhmm/hmm.py
@@ -104,7 +104,7 @@ class HMM(object):
     def _forwardPass(self,y,A,phi,pi0=None):
         
         '''
-        Computes forward pass of Expectation Maximization (EM) algorithm.
+        Computes forward pass of Expectation Maximization (EM) algorithm; first half of .
         
         Parameters
         ----------
@@ -147,7 +147,7 @@ class HMM(object):
     def _backwardPass(self,y,A,phi,alpha,cs):
         
         '''
-        Computes backward pass of Expectation Maximization (EM) algorithm.
+        Computes backward pass of Expectation Maximization (EM) algorithm; second half of "E-step".
         
         Parameters
         ----------
@@ -175,10 +175,113 @@ class HMM(object):
             beta_prior = np.multiply(beta[i+1],phi[:,int(y[i+1])]) # propogate uncertainty backward
             beta[i] = (A@beta_prior)/cs[i+1]
             
-        pBack = np.multiply(alpha,beta) # posterior after backward pass
+        pBack = np.multiply(alpha,beta) # posterior after backward pass -> alpha_hat(z_n)*beta_hat(z_n)
         zhatBack = np.argmax(pBack,axis=1) # decode from likelihoods only
         
         return pBack,beta,zhatBack
+    
+    def _updateTransitions():
+        
+                # CALCULATE XIS
+        xis_sum = []
+        xis = np.zeros((nT-1,nStates,nStates))
+        
+        for i in np.arange(0,nT-1):
+            if method == "GLM":
+                bm = np.multiply(bb[i+1,:],M[:,i,int(yy[i+1])])
+            elif method == "standard":
+                bm = np.multiply(bb[i+1,:],M[int(yy[i+1]),:])
+            a = np.reshape(aa[i,:],(nStates,1))
+            abm = np.multiply(bm,a)
+            abmA = np.multiply(abm,A)/cs[i+1]
+        
+            xis[i,:,:] = abmA
+            xis_sum.append(np.sum(abmA))
+            
+
+        assert np.round(sum(gammas[0]),5) == 1, "Sum of gammas does not equal 1" 
+        
+        
+        # new transition matrix
+        xis_n = np.reshape(np.sum(xis,axis=0),(nStates,nStates)) # sum_N xis
+        xis_kn = np.reshape(np.sum(np.sum(xis,axis=0),axis=1),(nStates,1)) # sum_k sum_N xis
+        A_jk = xis_n/xis_kn
+        
+        return A
+        
+    def _updateObservations():
+        
+        x_ni = np.zeros((nT,nObs))
+        for i in np.arange(nT):
+            x_ni[i,int(yy[i])] = 1 # assign 1 to index assoc. with observation
+
+            
+        if method == "standard":
+        
+            # new observation matrix
+            gammaxni_n = np.dot(gammas.T,x_ni).T # sum_N gamma(z_nk)*x_ni
+            gamma_n = np.sum(gammas,axis=0) # sum_N gamma(z_nk)
+            mu_ik = gammaxni_n/gamma_n
+            
+            M = mu_ik # observation matrix
+            
+        if method == "GLM":
+            
+            ## run GLM to get new observation matrix, weights, covariance of weights
+            mu_ik = np.zeros((nStates,nT,nObs))
+            w_updated = np.zeros((nStates,nFeat,nObs))
+            variances = np.zeros((nStates,nFeat*(nObs-1)))
+            for zn in np.arange(nStates):
+                mu_ik[zn,:,:], w_updated[zn,:,:], logl, _ = GLM.weighted_optimize_weights(w_init[zn,:,:],x_true,y_true,gammas[:,zn],"L-BFGS-B",bias=0, compHess = False, gaussPrior = gaussian_prior) 
+                
+            M =  mu_ik # update array of observation matrices
+            w_init = w_updated # update weights
+        
+        return phi
+        
+    def _updateInitStates(gammas):
+        '''
+        Computes the updated initial state probabilities as part of the M-step of the EM algorithm.
+
+        Parameters
+        ----------
+        gammas : nxk matrix of the posterior probabilities of the latent states
+
+        Returns
+        -------
+        pi0 : kx1 vector of initial latent state probabilities (for t=1)
+
+        '''
+        
+        pi0 = np.divide(gammas[0],sum(gammas[0])) # new initial latent state probabilities
+        
+        return pi0
+    
+    def _updateParams(gammas,beta,alpha,cs,A,fit_init_states = False):
+        
+        A = _updateTransitions():
+            
+        phi = _updateObservations():
+        
+        pi0 = _updateInitStates(gammas)
+        
+        # Store values in matrices
+        As[n,:,:] = A
+        if method == "standard":
+            Ms[n,:,:] = M
+        elif method == "GLM":
+            M_rearranged = np.zeros((nT,nObs,nStates))
+            for i in np.arange(0,nT):
+                M_rearranged[i,:,:] = M[:,i,:].T
+            
+            Ms[n,:,:,:] = M_rearranged
+            
+        ws[n,:,:,:] = w_updated
+            
+        logLikelihoods[n] = ll
+        
+        return A,phi,pi0
+
     
     def EM(self):
         

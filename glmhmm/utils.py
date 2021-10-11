31971c0526eca4e2157d48ae10416b6ef64ea6a2
irisstone <istone@princeton.edu>
2021-10-10 22:49:18 -0400

    update permute_states func to support observations, order method

--- glmhmm/utils.py
+++ glmhmm/utils.py
@@ -11,20 +11,59 @@ Miscellaneous helper functions for fitting and analyzing GLM/HMM code
 
 import numpy as np
 
-def permute_states(A):
+def permute_states(M,method='self-transitions',param='transitions',order=None):
     
-    'Permute states in order from highest to lowest self-transition value'
+    '''
+    Parameters
+    ----------
+    M : matrix of probabilities for input parameter (transitions, observations, or initial states)
+    Methods --- 
+        self-transitions : permute states in order from highest to lowest self-transition value (works
+             only with transition probabilities as inputs)
+        order : permute states according to a given order
+    param : specifies the input parameter
+    order : optional, specifies the order of permuted states for method=order
     
-    diags = np.diagonal(A) # get diagonal values for sorting
+    Returns
+    -------
+    M_perm : M permuted according to the specified method/order
+    order : the order of the permuted states
+    '''
     
-    order = np.flip(np.argsort(diags))
+    # check for valid method
+    method_list = {'self-transitions','order'}
+    if method not in method_list:
+        raise Exception("Invalid method: {}. Must be one of {}".
+            format(method, method_list))
+        
+    # sort according to transitions
+    if method =='self-transitions':
+        
+        if param != 'transitions':
+            raise Exception("Invalid parameter choice: self-transitions permutation method \
+                            requires transition probabilities as parameter function input")
+        diags = np.diagonal(M) # get diagonal values for sorting
+        
+        order = np.flip(np.argsort(diags))
+        
+        M_perm = np.zeros_like(M)
+        for i in range(M.shape[0]):
+            for j in range(M.shape[1]):
+                M_perm[i,j] = M[order[i],order[j]]
+                
+    # sort according to given order
+    if method == 'order':
+        if param=='transitions':
+            M_perm = np.zeros_like(M)
+            for i in range(M.shape[0]):
+                for j in range(M.shape[1]):
+                    M_perm[i,j] = M[order[i],order[j]]
+        if param=='observations':
+            M_perm = np.zeros_like(M)
+            for i in range(M.shape[0]):
+                M_perm[i,:] = M[order[i],:]
     
-    Aperm = np.zeros_like(A)
-    for i in range(A.shape[0]):
-        for j in range(A.shape[1]):
-            Aperm[i,j] = A[order[i],order[j]]
-    
-    return Aperm, order 
+    return M_perm, order 
 
 def find_best_fit(lls):
 

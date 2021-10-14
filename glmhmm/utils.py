23327a54842af38a2865f59dffee7b30fe077c1c
irisstone <istone@princeton.edu>
2021-10-14 18:46:09 -0400

    add compare_top_weights()

--- glmhmm/utils.py
+++ glmhmm/utils.py
@@ -11,7 +11,7 @@ Miscellaneous helper functions for fitting and analyzing GLM/HMM code
 
 import numpy as np
 
-def permute_states(M,method='self-transitions',param='transitions',order=None):
+def permute_states(M,method='self-transitions',param='transitions',order=None,ix=None):
     
     '''
     Parameters
@@ -31,7 +31,7 @@ def permute_states(M,method='self-transitions',param='transitions',order=None):
     '''
     
     # check for valid method
-    method_list = {'self-transitions','order'}
+    method_list = {'self-transitions','order','weight value'}
     if method not in method_list:
         raise Exception("Invalid method: {}. Must be one of {}".
             format(method, method_list))
@@ -66,9 +66,40 @@ def permute_states(M,method='self-transitions',param='transitions',order=None):
             M_perm = np.zeros_like(M)
             for i in range(M.shape[0]):
                 M_perm[i,:,:] = M[order[i],:,:]
+                
+    # sort by the value of a particular weight
+    if method == 'weight value':
+        if ix is None:
+            raise Exception("Index of weight ix must be specified for this method")
+        
+        order = np.argsort(M[:,ix])
+        
+        M_perm = np.zeros_like(M)
+        for i in range(M.shape[0]):
+            M_perm[i,:] = M[order[i],:]
     
     return M_perm, order 
+
 
 def find_best_fit(lls):
 
     return np.argmax(np.nanmax(lls,axis=1))
+
+def compare_top_weights(w,ixs,tol=0.05):
+    '''
+    compares the weights associated with the top lls from multiple glm-hmm fits and checks if each 
+    weight matches within the given tolerance
+    '''
+    
+    best_weights = w[ixs[0],:,:]
+    diff = np.zeros((len(ixs),w.shape[1],w.shape[2]))
+    for i in range(1,len(ixs)): # for each specified fit (associated with the top lls)
+        for j in range(w.shape[1]): # for each state
+            diff[i-1,j,:] = abs(best_weights[j,:] - w[ixs[i],j,:])
+            
+    if np.any(diff > tol):
+        print('One or more weights differ by more than the set tolerance. The largest difference was %.2f.' %(np.max(diff)))
+        print('Try changing the tolerance or decreasing the number of top fits to compare.')
+    else:
+        print('None of the weights differ by more than the set tolerance. The largest difference was %.2f.' %(np.max(diff)))
+        print('This confirms that the top fits (as specified) all converged on the same solution.')

fb0fb7a8755fbcc8ee2fc945b2851069374357d8
irisstone <istone@princeton.edu>
2021-10-14 18:21:34 -0400

    plot log-likelihood func

--- glmhmm/visualize.py
+++ glmhmm/visualize.py
@@ -5,11 +5,11 @@ Created on Mon Oct  4 17:20:42 2021
 
 @author: irisstone
 
-Functions for visualizing and plotting results related to glmhmm fitting codd
+Functions for visualizing and plotting results related to glmhmm fitting code
 """
 import matplotlib.pyplot as plt
 import numpy as np
-
+from glmhmm.utils import find_best_fit
     
 def plot_model_params(M,ax):
     
@@ -32,3 +32,36 @@ def plot_model_params(M,ax):
                 ax.text((j+1)/(J)-(1/(J+1)),((I-i)/I)-(1/(I+2)),'%.2f' %(M[i,j]),transform=ax.transAxes,fontsize=15,color=color)
             else:
                 ax.text(0.3,((I-i)/I)-(1/(I+2)),'%.2f' %(M[i,j]),transform=ax.transAxes,fontsize=15,color=color)
+
+def plot_loglikelihoods(lls,maxdiff,startix=5):
+    '''
+    Plot the trajectory of the log-likelihoods for multiple fits, identify how many top fits (nearly) match, and 
+    color those trajectories in the plot accordingly
+    '''
+    
+    # get the final ll for each fit
+    final_lls = np.array([np.amax(lls[i,~np.isnan(lls[i,:])]) for i in range(lls.shape[0])])
+    
+    # get the index of the top ll
+    bestix = find_best_fit(lls)
+    
+    # compute the difference between the top ll and all final lls
+    ll_diffs = final_lls[bestix] - final_lls
+    
+    # identify te fits where the difference from the top ll is less than maxdiff
+    top_matching_lls = lls[ll_diffs < maxdiff,:]
+    
+    # plot
+    plt.plot(lls.T[startix:], color='black')
+    plt.plot(top_matching_lls.T[startix:], color='red')
+    plt.xlabel('iterations of EM', fontsize=16)
+    plt.ylabel('log-likelihood', fontsize=16)
+    
+    return np.where(ll_diffs < maxdiff)[0] # return indices of best (matching) fits
+
+
+def plot_weights(w,ax,xlabels=None,colors=None):
+    
+    plt.plot()
+    
+    
